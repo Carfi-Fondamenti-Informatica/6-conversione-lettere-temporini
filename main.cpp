58d0895757401a6473f2cf3d03bf9b85b417d425
temporini <91788264+temporini@users.noreply.github.com>
2021-12-01 17:09:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+
+    char q, lettera = 0;
+    cin >> q;
+
+    if ( conversionecarattere(q, lettera) == true ){
+        cout << lettera << endl;
+    }else{
+        cout << "errore" << endl;
+    }
+
+
+    return 0;
 }
