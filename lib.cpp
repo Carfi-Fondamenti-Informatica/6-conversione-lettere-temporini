e9de2a934d482fb06038d8b4b02346bc32a0426e
temporini <91788264+temporini@users.noreply.github.com>
2021-12-01 17:08:59 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
+#include"lib.h"
 
+int conversionecarattere(char c, char &risultato){
+    bool carattere;
+
+    if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')){
+        carattere = true;
+
+        if(c >= 'A' && c <= 'Z'){
+            risultato = c +32;
+        }
+        if(c >= 'a' && c <= 'z'){
+            rissultato= c -32;
+        }
+
+    }else{
+        carattere = false;
+    }
+    return carattere;
+}
